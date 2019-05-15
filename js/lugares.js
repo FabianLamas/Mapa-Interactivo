310ea4f27c5df743ec2f9510703ef6baf1136377
fabianlamas <fabian.lamas2.0@gmail.com>
2019-05-15 19:23:21 -0300

    first commit

---
+++ js/lugares.js
@@ -0,0 +1,71 @@
+lugaresModulo = (function () {
+  var servicioLugares // Servicio para obtener lugares cercanos e información de lugares(como fotos, puntuación del lugar,etc).
+
+    // Completa las direcciones ingresadas por el usuario a y establece los límites
+    // con un círculo cuyo radio es de 20000 metros.
+  function autocompletar () {
+        /* Completar la función autocompletar(): autocompleta los 4 campos de texto de la
+        página (las direcciones ingresables por el usuario).
+        Para esto creá un círculo con radio de 20000 metros y usalo para fijar
+        los límites de la búsqueda de dirección. El círculo no se debe ver en el mapa. */
+        var radio = 20000;
+
+        var autocompleteAddress = new google.maps.places.Autocomplete(
+          document.getElementById('direccion'), {types: ['geocode']});
+
+        var autocompleteFrom = new google.maps.places.Autocomplete(
+          document.getElementById('desde'), {types: ['geocode']});
+
+        var autocompleteTo = new google.maps.places.Autocomplete(
+          document.getElementById('hasta'), {types: ['geocode']});
+
+        var autocompleteAdd = new google.maps.places.Autocomplete(
+          document.getElementById('agregar'), {types: ['geocode']});
+    
+        if(navigator.geolocation){
+          navigator.geolocation.getCurrentPosition( function (position) {
+            var geolocation = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            };
+            var circle = new google.maps.Circle(
+              {center: geolocation, radius: radio});
+              autocompleteAddress.setBounds(circle.getBounds());
+              autocompleteFrom.setBounds(circle.getBounds());
+              autocompleteTo.setBounds(circle.getBounds());
+              autocompleteAdd.setBounds(circle.getBounds());
+          });
+        }
+  }
+
+    // Inicializo la variable servicioLugares y llamo a la función autocompletar
+  function inicializar () {
+    servicioLugares = new google.maps.places.PlacesService(mapa)
+    autocompletar()
+  }
+
+    // Busca lugares con el tipo especificado en el campo de TipoDeLugar
+
+  function buscarCerca (posicion) {
+        /* Completar la función buscarCerca  que realice la búsqueda de los lugares
+    del tipo (tipodeLugar) y con el radio indicados en el HTML cerca del lugar
+    pasado como parámetro y llame a la función marcarLugares. */
+
+    var tipoDeLugar = document.getElementById('tipoDeLugar').value;
+    var radio = parseInt(document.getElementById('radioS').value);
+    
+    var request = {
+      location: posicion,
+      radius: radio,
+      type: [tipoDeLugar]
+    };
+
+    servicioLugares.nearbySearch(request, marcadorModulo.marcarLugares);
+    //servicioLugares.nearbySearch(request, marcadorModulo.marcarLugares(results, status));
+  }
+
+  return {
+    inicializar,
+    buscarCerca
+  }
+})()
